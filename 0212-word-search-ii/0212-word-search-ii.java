093419f71f0ac4ae6a57c20feef72c503523d70b
Gokul Krishnan YN <116831237+Krishjunier@users.noreply.github.com>
2026-01-14 14:06:28 +0530

    Time: 125 ms (84.42%), Space: 47 MB (73.38%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    TrieNode root = new TrieNode();
+    List<String> result = new ArrayList<>();
+
+    public List<String> findWords(char[][] board, String[] words) {
+        if (board == null || board.length == 0) return result;
+
+        for (String word : words) {
+            insert(word);
+        }
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dfs(board, i, j, root);
+            }
+        }
+        return result;
+    }
+
+    private void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (node.children[idx] == null)
+                node.children[idx] = new TrieNode();
+            node = node.children[idx];
+        }
+        node.word = word;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node) {
+        char c = board[i][j];
+        if (c == '#' || node.children[c - 'a'] == null)
+            return;
+
+        node = node.children[c - 'a'];
+
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null; // avoid duplicates
+        }
+
+        board[i][j] = '#';
+
+        if (i > 0) dfs(board, i - 1, j, node);
+        if (j > 0) dfs(board, i, j - 1, node);
+        if (i < board.length - 1) dfs(board, i + 1, j, node);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, node);
+
+        board[i][j] = c;
+    }
+}
